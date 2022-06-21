a9689f9997e6dea3e03e47ceb7859769496e0208
Shreedhar Pandey <sridhar.pandey1@gmail.com>
2022-06-21 16:36:44 -0500

    Added 3 more sorting algo

--- src/components/sorting/Bars.js
+++ src/components/sorting/Bars.js
@@ -3,13 +3,14 @@ import { Bar } from "./Bar";
 
 export const Bars = ({ count }) => {
   const [bars, setBars] = useState(new Array(count));
+  const [sortAlgo, setSortAlgo] = useState("bubble");
+
+  function handleSelect(event) {
+    setSortAlgo(event.target.value);
+  }
 
   useEffect(() => {
-    let tempBars = new Array(count);
-    for (let i = 0; i < count; i++) {
-      tempBars[i] = Math.ceil(Math.random() * 50);
-    }
-    setBars(tempBars);
+    reset();
   }, []);
 
   const barsEl = bars.map((val, idx) => {
@@ -17,44 +18,176 @@ export const Bars = ({ count }) => {
   });
 
   function sort() {
-    // setBars((prev) => [
-    //   ...prev.sort(function (a, b) {
-    //     return a - b;
-    //   }),
-
-    // ]);
-
-    bubbleSort([...bars]);
-    // setBars((prev) => bubbleSort([...prev]));
+    switch (sortAlgo) {
+      case "bubble":
+        bubbleSort([...bars]);
+        break;
+      case "insertion":
+        insertionSort([...bars]);
+        break;
+      case "merge":
+        mergeSort([...bars]);
+        break;
+      case "quick":
+        iterativeQuickSort([...bars]);
+        break;
+    }
   }
 
   async function bubbleSort(arr) {
-    var i, j;
-    var len = arr.length;
+    let isSwapped = false;
 
-    var isSwapped = false;
-
-    for (i = 0; i < len; i++) {
-      isSwapped = false;
-
-      for (j = 0; j < len; j++) {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length - 1; j++) {
         if (arr[j] > arr[j + 1]) {
-          var temp = arr[j];
+          let temp = arr[j];
           arr[j] = arr[j + 1];
           arr[j + 1] = temp;
           isSwapped = true;
-        }
-        setBars([...arr]);
-        await delay(25);
-      }
 
-      // IF no two elements were swapped by inner loop, then break
+          setBars([...arr]);
+          await delay(25);
+        }
+      }
       if (!isSwapped) {
         break;
       }
     }
-    // return arr;
   }
+
+  async function insertionSort(arr) {
+    let i = 1;
+    let j;
+    while (i < arr.length) {
+      j = i;
+      while (j >= 0 && arr[j - 1] > arr[j]) {
+        let temp = arr[j - 1];
+        arr[j - 1] = arr[j];
+        arr[j] = temp;
+        j = j - 1;
+
+        setBars([...arr]);
+        await delay(100);
+      }
+      i += 1;
+    }
+  }
+
+  const swap = (arr, left, right) => {
+    const temp = arr[left];
+    arr[left] = arr[right];
+    arr[right] = temp;
+  };
+
+  const partitionHigh = async (arr, low, high) => {
+    //Pick the first element as pivot
+    let pivot = arr[high];
+    let i = low;
+
+    //Partition the array into two parts using the pivot
+    for (let j = low; j < high; j++) {
+      if (arr[j] <= pivot) {
+        swap(arr, i, j);
+        i++;
+
+        setBars([...arr]);
+        await delay(100);
+      }
+    }
+
+    swap(arr, i, high);
+    setBars([...arr]);
+    await delay(100);
+    //Return the pivot index
+    return i;
+  };
+
+  const iterativeQuickSort = async (arr) => {
+    //Stack for storing start and end index
+    let stack = [];
+
+    //Get the start and end index
+    let start = 0;
+    let end = arr.length - 1;
+
+    //Push start and end index in the stack
+    stack.push({ x: start, y: end });
+
+    //Iterate the stack
+    while (stack.length) {
+      //Get the start and end from the stack
+      const { x, y } = stack.shift();
+
+      //Partition the array along the pivot
+      const PI = await partitionHigh(arr, x, y);
+      setBars([...arr]);
+      await delay(100);
+      //Push sub array with less elements than pivot into the stack
+      if (PI - 1 > x) {
+        stack.push({ x: x, y: PI - 1 });
+      }
+
+      //Push sub array with greater elements than pivot into the stack
+      if (PI + 1 < y) {
+        stack.push({ x: PI + 1, y: y });
+      }
+    }
+  };
+
+  const mergeSort = async (arr) => {
+    //Create two arrays for sorting
+    let sorted = Array.from(arr);
+    let n = sorted.length;
+    let buffer = new Array(n);
+
+    for (let size = 1; size < n; size *= 2) {
+      for (let leftStart = 0; leftStart < n; leftStart += 2 * size) {
+        //Get the two sub arrays
+        let left = leftStart,
+          right = Math.min(left + size, n),
+          leftLimit = right,
+          rightLimit = Math.min(right + size, n);
+
+        //Merge the sub arrays
+        merge(left, right, leftLimit, rightLimit, sorted, buffer);
+      }
+
+      //Swap the sorted sub array and merge them
+      let temp = sorted;
+      sorted = buffer;
+      buffer = temp;
+      setBars([...sorted]);
+      await delay(1000);
+    }
+
+    setBars([...sorted]);
+    await delay(1000);
+
+    return sorted;
+  };
+
+  const merge = async (left, right, leftLimit, rightLimit, sorted, buffer) => {
+    let i = left;
+
+    //Compare the two sub arrays and merge them in the sorted order
+    while (left < leftLimit && right < rightLimit) {
+      if (sorted[left] <= sorted[right]) {
+        buffer[i++] = sorted[left++];
+      } else {
+        buffer[i++] = sorted[right++];
+      }
+    }
+
+    //If there are elements in the left sub arrray then add it to the result
+    while (left < leftLimit) {
+      buffer[i++] = sorted[left++];
+    }
+
+    //If there are elements in the right sub array then add it to the result
+    while (right < rightLimit) {
+      buffer[i++] = sorted[right++];
+    }
+  };
 
   function delay(delayTime) {
     return new Promise((resolve) => {
@@ -64,10 +197,25 @@ export const Bars = ({ count }) => {
     });
   }
 
+  function reset() {
+    let tempBars = new Array(count);
+    for (let i = 0; i < count; i++) {
+      tempBars[i] = Math.ceil(Math.random() * 50);
+    }
+    setBars(tempBars);
+  }
+
   return (
     <div>
       <div className="bars">{barsEl}</div>
       <button onClick={sort}>Sort</button>
+      <button onClick={reset}>Reset</button>
+      <select onChange={handleSelect}>
+        <option value="bubble">Bubble Sort</option>
+        <option value="insertion">Insertion Sort</option>
+        <option value="merge">Merge Sort</option>
+        <option value="quick">Quick Sort</option>
+      </select>
     </div>
   );
 };
