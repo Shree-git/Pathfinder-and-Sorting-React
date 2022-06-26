c2e62b7c2d0a9e79d932b599480cba0ee3bfb305
Shreedhar Pandey <sridhar.pandey1@gmail.com>
2022-06-26 01:32:10 -0500

    cors fix 4

--- backend/app.js
+++ backend/app.js
@@ -9,11 +9,11 @@ const corsOptions = {
 
 const PORT = process.env.PORT || 3005;
 app.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Origin",
     "https://pathfinder-and-sorting-react.netlify.app"
   ); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
